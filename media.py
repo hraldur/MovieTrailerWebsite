1a97ed71beb410131df3be9cbccd76ba674ea165
hraldur <hrj28@hi.is>
2017-02-09 21:17:41 +0100

    Update media.py

--- media.py
+++ media.py
@@ -1,7 +1,13 @@
+"""media.py: stores movie data."""
+__author__ = "Hrafnhildur Jonasdottir"
+
+
 class Movie():
-	
-	#Create Movie object
-	def __init__(self, title, poster, trailer):
-		self.title = title
-		self.poster_image_url = poster
-		self.trailer_youtube_url = trailer
+    """
+    This class stores movie data such as a movie title,
+    a poster url and a trailer url
+    """
+    def __init__(self, title, poster, trailer):
+        self.title = title
+        self.poster_image_url = poster
+        self.trailer_youtube_url = trailer
